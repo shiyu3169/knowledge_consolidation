37e59cbf6acf715386f7cf4c33b6c1f2cb88b921
Shiyu Wang <shiyu3169@gmail.com>
2023-02-11 21:36:39 -0800

    bubble sort

---
+++ Front-end/BFE/bubbleSort.js
@@ -0,0 +1,31 @@
+/* Even for Front-End Engineer, it is a must to understand how basic sorting algorithms work.
+
+Now you are asked to implement Bubble Sort, which sorts an integer array in ascending order.
+
+Do it in-place, no need to return anything.
+
+Follow-up
+
+What is time cost for average / worst case ? Is it stable? */
+
+/**
+ * @param {number[]} arr
+ */
+function bubbleSort(array) {
+  let isSorted = false
+  let lastUnsorted = array.length - 1
+
+  while (!isSorted) {
+    isSorted = true
+    for (let i = 0; i < lastUnsorted; i++) {
+      if (array[i] > array[i + 1]) {
+        ;[array[i], array[i + 1]] = [array[i + 1], array[i]]
+        isSorted = false
+      }
+    }
+    lastUnsorted--
+  }
+
+  return array
+}
+a
