2c6e3976d571cae42a111dc20611d11d5ccefe3e
Shiyu Wang <shiyu3169@gmail.com>
2023-02-11 22:46:01 -0800

    insertion sort

---
+++ Front-end/BFE/insertionSort.js
@@ -0,0 +1,21 @@
+/* Even for Front-End Engineer, it is a must to understand how basic sorting algorithms work.
+
+Now you are asked to implement Insertion Sort, which sorts an integer array in ascending order.
+
+Do it in-place, no need to return anything.
+
+Follow-up
+
+What is time cost for average / worst case ? Is it stable? */
+
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let current = array[i]
+    let j = i - 1
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j]
+      j--
+    }
+    array[j + 1] = current
+  }
+}
