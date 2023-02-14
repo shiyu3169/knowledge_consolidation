8008f3213cd2c15cc92d07ef5f580425dc6795e5
Shiyu Wang <shiyu3169@gmail.com>
2023-02-13 16:39:27 -0800

    51

---
+++ Front-end/BFE/elementAfterIndex.js
@@ -0,0 +1,34 @@
+/* This is a variation of 37. implement Binary Search (unique).
+
+Your are given a sorted ascending array of number, but might have duplicates, you are asked to return the element right after last appearance of a target number.
+
+If not found return undefined.
+
+note
+
+Please don't use Array.prototype.lastIndexOf(), it is not our goal. */
+
+/**
+ * @param {number[]} arr - ascending array with duplicates
+ * @param {number} target
+ * @return {number}
+ */
+function elementAfter(arr, target) {
+  let left = 0,
+    right = arr.length - 1
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+    if (arr[mid] === target) {
+      if (mid === 0) return undefined
+      if (arr[mid + 1] !== target) return arr[mid + 1]
+      left = mid + 1
+      continue
+    }
+    if (target < arr[mid]) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+  return undefined
+}
