d4687473af60cf800ad650400d31e0dd1b1ddcf4
Shiyu Wang <shiyu3169@gmail.com>
2023-02-27 17:32:18 -0800

    Add localStorage with expiration implementation to store data with expiration time

---
+++ Front-end/BFE/Javascript Coding Questions/135. localStorage with expiration.js
@@ -0,0 +1,30 @@
+window.myLocalStorage = {
+  getItem(key) {
+    try {
+      const entry = JSON.parse(localStorage.getItem(key))
+      if (entry.expiredAt && entry.expiredAt <= Date.now()) {
+        this.removeItem(key)
+        return null
+      }
+      return entry.value
+    } catch (e) {
+      return null
+    }
+  },
+
+  setItem(key, value, maxAge) {
+    const entry = {
+      value,
+      expiredAt: !isNaN(maxAge) ? Date.now() + maxAge : undefined,
+    }
+    localStorage.setItem(key, JSON.stringify(entry))
+  },
+
+  removeItem(key) {
+    localStorage.removeItem(key)
+  },
+
+  clear() {
+    localStorage.clear()
+  },
+}
