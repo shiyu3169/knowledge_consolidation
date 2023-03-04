50db80161920a6f87c3a3e76f677f1deaf7cb5ca
Shiyu Wang <shiyu3169@gmail.com>
2023-03-03 19:54:24 -0800

    159

---
+++ Front-end/BFE/Javascript Coding Questions/159. implement promisify().js
@@ -0,0 +1,17 @@
+/**
+ * @param {(...args) => void} func
+ * @returns {(...args) => Promise<any}
+ */
+function promisify(func) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      func.call(this, ...args, (error, data) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(data)
+        }
+      })
+    })
+  }
+}
