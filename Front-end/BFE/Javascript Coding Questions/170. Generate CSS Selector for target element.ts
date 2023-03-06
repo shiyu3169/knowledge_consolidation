ac217c0378bbd3aa84d23e0ecff188c9869b37e0
Shiyu Wang <shiyu3169@gmail.com>
2023-03-05 17:07:58 -0800

    170

---
+++ Front-end/BFE/Javascript Coding Questions/170. Generate CSS Selector for target element.ts
@@ -0,0 +1,14 @@
+// This is a JavaScript coding problem from BFE.dev
+
+function generateSelector(root: HTMLElement, target: HTMLElement): string {
+  const path = []
+  let curNode = target
+  while (curNode && curNode !== root) {
+    path.push(
+      curNode.id ? `#${curNode.id}` : curNode.tagName.toLocaleLowerCase(),
+    )
+    curNode = curNode.parentElement!
+  }
+  if (curNode !== root) throw new Error('You are looking for something?')
+  return path.reverse().join(' > ')
+}
